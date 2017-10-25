407231cd2278ad5bb83ba15db747ff5a1c8f0736
PatQz <patqzr@gmail.com>
2017-10-24 20:17:05 -0700

    linea-circunferencia-elipse

---
+++ Linea.cpp
@@ -0,0 +1,98 @@
+#include <stdio.h>
+#include <SDL2/SDL.h>
+
+int main( int argc, char* args[] ){
+    int X1,Y1,X2,Y2;
+    X1=0;
+    X2=600;
+    Y1=0;
+    Y2=600;
+    int tp=600;
+    double dY, dX,k,X,Y,av,avR,avI;
+    int IncYi,IncXi,IncXr,IncYr;
+    //Event handler
+    SDL_Event e;
+    //gWindow = SDL_CreateWindow( "SDL Tutorial", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN );
+   
+   if (SDL_Init(SDL_INIT_VIDEO) == 0) {
+        SDL_Event event;
+        SDL_Window* window = NULL;
+        SDL_Renderer* renderer = NULL;
+
+        if (SDL_CreateWindowAndRenderer(tp,tp, 0, &window, &renderer) == 0) {
+            SDL_SetRenderDrawColor(renderer, 0, 0, 0, 0);
+            SDL_RenderClear(renderer);
+             SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255);
+            SDL_RenderDrawLine(renderer, 300,0,300,600);
+            SDL_RenderDrawLine(renderer, 0,300,600,300);
+            SDL_SetRenderDrawColor(renderer, 255, 0,255, 255);
+                
+        dY=(Y2-Y1);
+        dX=(X2-X1);
+         // 1 - Incrementos para las secciones con avance inclinado
+        if(dY>=0){
+            IncYi = 1;
+        }else{
+            IncYi = -1;
+        }
+
+        if(dX>=0){
+            IncXi = 1;
+        }else{
+            IncXi = -1;
+        }
+        // 2 - Incrementos para las secciones con avance recto:
+        if(dX>=dY){
+            IncYr = 0;
+            IncXr = IncXi;
+        }else{
+            IncXr = 0;
+            IncYr = IncYi;
+        // Cuando dy es mayor que dx, se intercambian, para reutilizar el mismo bucle.
+        // ver octantes blancos en la imagen encima del código
+            k = dX;
+            dX = dY;
+            dY = k;
+        }
+
+        // 3  - Inicializar valores (y de error).
+        X = X1;
+        Y = Y1; 
+        avR = (2 * dY);
+        av = (avR - dX);
+        avI = (av - dX);
+
+        // 4  - Bucle para el trazado de las línea
+            while(X<=X2){
+            //for (int i = 0; i < tp; ++i){
+                SDL_RenderDrawPoint(renderer, X, Y);
+                if(av>=0){
+                    X = (X + IncXi);     // X aumenta en inclinado.
+                    Y = (Y + IncYi);    // Y aumenta en inclinado.
+                    av = (av + avI);     // Avance Inclinado
+                }else{
+                    X = (X + IncXr);     // X aumenta en recto.
+                    Y = (Y + IncYr);     // Y aumenta en recto.
+                    av = (av + avR);     // Avance Recto
+                }
+            }
+
+        SDL_RenderPresent(renderer);
+        while (1) {
+            if (SDL_PollEvent(&event) && event.type == SDL_QUIT)
+            break;
+        }
+        
+
+        if (renderer) {
+            SDL_DestroyRenderer(renderer);
+        }
+        if (window) {
+            SDL_DestroyWindow(window);
+                }
+        }
+     }
+
+    SDL_Quit();
+	return 0;
+}
---
+++ circunferencia.cpp
@@ -0,0 +1,87 @@
+#include <stdio.h>
+#include <SDL2/SDL.h>
+#include <math.h>
+
+int main( int argc, char* args[] ){
+    int X1,Y1,X2,Y2;
+    int tp=600;
+    double dY, dX,k,X,Y,av,avR,avI,d1,d2,u,v;
+    int IncYi,IncXi,IncXr,IncYr;
+
+    //Event handler
+    SDL_Event e;
+    //gWindow = SDL_CreateWindow( "SDL Tutorial", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN );
+   
+   if (SDL_Init(SDL_INIT_VIDEO) == 0) {
+        SDL_Event event;
+        SDL_Window* window = NULL;
+        SDL_Renderer* renderer = NULL;
+
+        if (SDL_CreateWindowAndRenderer(tp,tp, 0, &window, &renderer) == 0) {
+            SDL_SetRenderDrawColor(renderer, 0, 0, 0, 0);
+            SDL_RenderClear(renderer);
+            SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255);
+            SDL_RenderDrawLine(renderer, 300,0,300,600);
+            SDL_RenderDrawLine(renderer, 0,300,600,300);
+            SDL_SetRenderDrawColor(renderer, 255, 0,255, 255);
+            X1=200; //a
+            Y1=200; //b
+
+            X=0;
+            Y=Y1;
+            d1=pow(Y1,2)-(pow(X1,2)*Y1) + (pow(X1,2)/4.0); //valor inicial de dF(1,b-1/2)
+            SDL_RenderDrawPoint(renderer, X+300, Y+300);
+            SDL_RenderDrawPoint(renderer, -X+300, Y+300);
+            SDL_RenderDrawPoint(renderer, X+300, -Y+300);
+            SDL_RenderDrawPoint(renderer, -X+300, -Y+300);
+
+            while((pow(X1,2)*(Y-(0.5)))>(pow(Y1,2)*(X+1))){
+                if(d1<0){
+                    d1=d1+pow(Y1,2)*((2*X)+3);
+                    X=X+1;
+                }else{
+                    d1=d1+pow(Y1,2)*((2*X)+3) + pow(X1,2)*((-2*Y)+2);
+                    X=X+1;
+                    Y=Y-1;
+                }
+                SDL_RenderDrawPoint(renderer, X+300, Y+300);
+                SDL_RenderDrawPoint(renderer, -X+300, Y+300);
+                SDL_RenderDrawPoint(renderer, X+300, -Y+300);
+                SDL_RenderDrawPoint(renderer, -X+300, -Y+300);
+            }
+
+            d2=pow(Y1,2)*pow((X+(0.5)),2)+(pow(X1,2)*pow((Y-1),2))-(pow(X1,2)*pow(Y1,2));
+
+            while(Y>0){
+                if(d2<0){
+                    d2=d2+pow(Y1,2)*((2*X)+2) + pow(X1,2)*((-2*Y)+3);
+                    X=X+1;
+                    Y=Y-1;
+                }else{
+                    d2=d2+pow(X1,2)*((-2*Y)+3);
+                    Y=Y-1;
+                }
+                SDL_RenderDrawPoint(renderer, X+300, Y+300);
+                SDL_RenderDrawPoint(renderer, -X+300, Y+300);
+                SDL_RenderDrawPoint(renderer, X+300, -Y+300);
+                SDL_RenderDrawPoint(renderer, -X+300, -Y+300);
+            }
+
+        SDL_RenderPresent(renderer);
+        while (1) {
+            if (SDL_PollEvent(&event) && event.type == SDL_QUIT)
+            break;
+        }
+        
+
+        if (renderer) {
+            SDL_DestroyRenderer(renderer);
+        }
+        if (window) {
+            SDL_DestroyWindow(window);
+                }
+        }
+     }
+    SDL_Quit();
+	return 0;
+}
